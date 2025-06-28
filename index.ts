e0ad969cd07a787a447ff0e85917261cfc934023
jasmin <jajalorino@gmail.com>
2025-06-28 11:30:46 +0800

    added looping of arrays

--- index.ts
+++ index.ts
@@ -40,3 +40,49 @@ interface Banana {
 
 const listOfBananas: Banana[] = [{id:1, expiryDate: new Date(), isFresh: true}, {id:2, expiryDate: new Date(), isFresh: false}]; // array of Banana objects
 
+listOfBananas.push({id:3, expiryDate: new Date(), isFresh: true}); // pushing a new Banana object into the array
+
+listOfBananas.map((banana) => {
+    return banana.id; // mapping over the array to return an array of ids
+})
+
+
+// inherit
+interface Type1 {
+    name: string;
+}
+
+interface Type2 {
+    age: number;
+}
+
+// CombinedType inherits from Type1 and Type2 (interface)
+interface CombinedType extends Type1, Type2 {
+    gender: string; // additional property
+}
+
+// CombinedType2 inherits from Type1 and Type2 (type)
+type CombinedType2 = Type1 & Type2 & {
+    gender: string; // additional property
+}
+
+const combinedData: CombinedType = {
+    name: 'jasmin',
+    age: 30,
+    gender: 'female'
+};
+
+const combinedData2: CombinedType2 = {
+    name: 'jasmin',
+    age: 30,
+    gender: 'bakla'
+};
+
+
+type Test = {
+    orderBy: 'DESC' | 'desc' | 'ASC' | 'asc'; // union type for order
+}
+
+const test: Test = {
+    orderBy: 'DESC' // can be either DESC, desc, ASC, or asc
+};
