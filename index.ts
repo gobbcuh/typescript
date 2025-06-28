c5f185f781488b3dadbd42652406e4046f2c31a4
jasmin <jajalorino@gmail.com>
2025-06-28 09:32:31 +0800

    added type

--- index.ts
+++ index.ts
@@ -2,7 +2,15 @@ interface IUser {
     name: string; // This is a user interface
 }
 
+type TUser = {
+    name: string; // This is a user type
+}
+
 const user: IUser = {
     name: 'jasmin' // This is a user object
 
+};
+
+const user2: TUser = {
+    name: 'jasmin' // This is a user object
 };
